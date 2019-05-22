6962ef5ea3dfce8babe17e4ac0401c87525d5a04
wdlbanana <670251506@qq.com>
2019-05-23 05:06:14 +0800

    my

---
+++ src/main.js
@@ -0,0 +1,27 @@
+//入口文件
+import  Vue from 'vue'
+
+//导入app根组件
+import app from './App.vue'
+
+// 导入 MUI 的样式
+import './lib/mui/css/mui.min.css'
+
+// 按需导入 Mint-UI 中的组件
+import {Header} from 'mint-ui'
+Vue.component(Header.name,Header)
+
+
+
+
+
+
+
+
+
+
+
+var vm = new Vue({
+  el:'#app',
+  render:c =>c(app)
+})
